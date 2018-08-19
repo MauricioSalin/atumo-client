60a8f0e53d26e02124f3ec89a381b23dccc23537
Diego <Diego@admins-MacBook-Pro.local>
2018-08-19 02:09:59 -0300

    Add demo page and base_components/base_input

--- src/Demo.js
+++ src/Demo.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import BaseInput from './base_components/base_input/input';
+
+const inputCallback = val => (
+  console.log(val)
+);
+
 const demo = () => (
   <div>
-    Pagína de Demonstração dos componentes base!
+    <h1>Pagína de Demonstração dos componentes base!</h1>
+    <div style={{ margin: '20px' }}>
+      <h2>Base input</h2>
+      <BaseInput
+        placeholder="Teste"
+        onTextChange={inputCallback}
+        type="text"
+      />
+    </div>
   </div>
 );
 
