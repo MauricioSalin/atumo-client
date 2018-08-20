b2cbaa99e0dbc3152163d25e1d986604383e37a3
Diego <Diego@admins-MacBook-Pro.local>
2018-08-20 00:13:18 -0300

    base_button-3: Add base button, Font Awesome

--- src/Demo.js
+++ src/Demo.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import BaseInput from './base_components/base_input/input';
+import BaseButton from './base_components/base_button/button';
 
 const inputCallback = val => (
   console.log(val)
+);
+
+const buttonCallback = () => (
+  console.log('Button was clicked')
 );
 
 const demo = () => (
@@ -15,6 +20,34 @@ const demo = () => (
         placeholder="Teste"
         onTextChange={inputCallback}
         type="text"
+      />
+    </div>
+    <div>
+      <h2>Base button</h2>
+      <BaseButton
+        name="Primary"
+        click={buttonCallback}
+      />
+      <BaseButton
+        name="Warning"
+        type="warning"
+        click={buttonCallback}
+      />
+      <BaseButton
+        name="Danger"
+        type="danger"
+        click={buttonCallback}
+      />
+      <BaseButton
+        name="With icon"
+        icon="fa fa-plus fa-2x"
+        click={buttonCallback}
+      />
+      <BaseButton
+        name="With icon and Disabled"
+        icon="fa fa-plus fa-2x"
+        disabled
+        click={buttonCallback}
       />
     </div>
   </div>
