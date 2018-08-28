cfbedfc71f19d491a3a59c4258cb9bcad4d5ff89
Diego Santos <diegonogueira.santos@gmail.com>
2018-08-28 10:25:59 -0300

    ssh -v

--- scripts/digitalOceanDeploy.sh
+++ scripts/digitalOceanDeploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo apt-get update -y && apt-get install sshpass -y
-sudo sshpass -p largatomama root@142.93.54.182
+ssh -v root@142.93.54.182
 
 git config --global user.email "diegonogueira.santos@gmail.com"
 git config --global user.name "Diego Santos"
