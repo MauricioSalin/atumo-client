08e2c3235b868d6b4b8d7419f20e746122d14c51
Diego Santos <diegonogueira.santos@gmail.com>
2018-08-30 21:32:56 -0300

    test

--- src/App.js
+++ src/App.js
@@ -8,13 +8,6 @@ const App = () => (
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
     </header>
-    <p className="App-intro">
-      To get started, edit
-      {' '}
-      <code>src/App.js</code>
-      {' '}
-and save to reload.
-    </p>
   </div>
 );
 
